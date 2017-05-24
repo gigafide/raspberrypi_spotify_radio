398186cbda889ba6498db565aefa3d1cceaa466e
gigafide <gigafide@gmail.com>
2017-05-24 17:31:06 -0400

    Removed dependence on MPD
    Removed calls that required python-mpd2. Instead, using os.system commands to adjust volume.

--- volume.py
+++ volume.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python
 #Control MPD Client volume using a physical rotary encoder
 #pip install mopidy-spotify
-#pip install python-mpd2
 
-#import dependencies
-from mpd import MPDClient
 from RPi import GPIO
 from time import sleep
-
-#declare variables
-client = MPDClient()
+import os
 
 clk = 17
 dt = 27
 
-#set GPIO modes
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-#initialize counter, last click state, and starting volume.
 counter = 0
 clkLastState = GPIO.input(clk)
 currVolume = 50
 
-#try to connect to MPD server until connected
-connected = False
-while not connected:
-        try:
-                client.connect("localhost", 6600)
-                client.setvol(currVolume)
-                connected = True
-        except:
-                pass
-
-#create a loop to check if knob state has been changed
-#if so, adjust the volume accordingly
 try:
         while True:
                 clkState = GPIO.input(clk)
@@ -45,10 +26,9 @@ try:
                                 currVolume += 5
                         else:
                                 currVolume -= 5
-                        client.setvol(currVolume)
+#                       print currVolume
+                        os.system("mpc volume " + str(currVolume))
                         clkLastState = clkState
                         sleep(0.01)
 finally:
         GPIO.cleanup
-        client.close()
-        client.disconnect()
